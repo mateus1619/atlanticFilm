3be43ba2c3bbef78422b7ac0883a57e0c0452913
mateus1619 <matheusouza1619@gmail.com>
2023-04-09 22:25:48 -0300

    Add routes

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+// Controllers
+
+use App\Controller\Anime;
 use App\Controller\Home;
+use App\Controller\Movie;
+use App\Controller\Series;
 use Slim\Routing\RouteCollectorProxy;
 
 $app->group('', function(RouteCollectorProxy $router) {
+
     $router->get('/', [Home::class, 'show']);
+    $router->get('/movie', [Movie::class, 'show']);
+    $router->get('/series', [Series::class, 'show']);
+    $router->get('/anime', [Anime::class, 'show']);
 });
